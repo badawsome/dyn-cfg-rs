b4bc3d493e1f65a936f1cc39586b3055a4e150b2
badcw <badcw123@gmail.com>
2025-12-16 19:18:52 +0800

    bump version

--- dyn_cfg/src/lib.rs
+++ dyn_cfg/src/lib.rs
@@ -4,6 +4,7 @@ pub mod prelude {
     pub use dyn_cfg_macros::dynamic_config;
     pub use faststr;
     pub use futures::StreamExt;
+    pub use std::str::FromStr;
     pub use tokio::{self, task::JoinSet};
     pub use tracing;
 }
